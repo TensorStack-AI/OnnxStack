2e6f8bda6d4486ecfd6f17d234f2daadabfb9cb9
sa_ddam213 <sa_ddam213@live.com>
2023-11-09 16:44:40 +1300

    Create new settings view for UI related configuration

---
+++ OnnxStack.UI/Views/Settings.xaml.cs
@@ -0,0 +1,42 @@
+﻿using OnnxStack.UI.Models;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OnnxStack.UI.Views
+{
+    /// <summary>
+    /// Interaction logic for Settings.xaml
+    /// </summary>
+    public partial class Settings : UserControl, INavigatable, INotifyPropertyChanged
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Settings"/> class.
+        /// </summary>
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+
+        public Task NavigateAsync(ImageResult imageResult)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+        #endregion
+    }
+
+
+}
