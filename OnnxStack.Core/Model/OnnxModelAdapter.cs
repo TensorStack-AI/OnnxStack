de2b11596b765cd9cdba6d6e9b21ef368e030ad6
sa_ddam213 <sa_ddam213@live.com>
2023-11-11 07:12:34 +1300

    LoRA to use  IOnnxModel

--- OnnxStack.Core/Model/OnnxModelAdapter.cs
+++ OnnxStack.Core/Model/OnnxModelAdapter.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OnnxStack.Core.Config;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,8 @@ using System.Threading.Tasks;
 
 namespace OnnxStack.Core.Model
 {
-    public class OnnxModelAdapter
+    public class OnnxModelAdapter : IOnnxModel
     {
-        // TODO: Load LoRA file
+        public string Name { get; set; }
     }
 }
