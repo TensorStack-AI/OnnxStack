1e89247fc099ee9edd2c00c8c1130dbf953d75aa
sa_ddam213 <sa_ddam213@live.com>
2023-11-12 09:08:49 +1300

    InpaintLegacyDiffuser

---
+++ OnnxStack.StableDiffusion/Diffusers/LatentConsistency/InpaintLegacyDiffuser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnnxStack.StableDiffusion.Diffusers.LatentConsistency
+{
+    internal class InpaintLegacyDiffuser
+    {
+    }
+}
