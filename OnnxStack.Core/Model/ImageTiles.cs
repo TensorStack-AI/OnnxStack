2ef4574010011dd84536f6712f88211de97f1947
sa_ddam213 <sa_ddam213@live.com>
2024-03-07 14:36:12 +1300

    Methods to split and join a tensor into tiles

---
+++ OnnxStack.Core/Model/ImageTiles.cs
@@ -0,0 +1,6 @@
+﻿using Microsoft.ML.OnnxRuntime.Tensors;
+
+namespace OnnxStack.Core.Model
+{
+    public record ImageTiles(DenseTensor<float> Tile1, DenseTensor<float> Tile2, DenseTensor<float> Tile3, DenseTensor<float> Tile4);
+}
