208187f647db25e6c02ff6cb150c55d0ff8b3605
sa_ddam213 <sa_ddam213@live.com>
2024-03-31 08:13:14 +1300

    StableCascade Pipeline scaffold

---
+++ OnnxStack.StableDiffusion/Pipelines/StableCascadePipeline.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Extensions.Logging;
+using OnnxStack.Core;
+using OnnxStack.Core.Config;
+using OnnxStack.StableDiffusion.Config;
+using OnnxStack.StableDiffusion.Diffusers;
+using OnnxStack.StableDiffusion.Diffusers.InstaFlow;
+using OnnxStack.StableDiffusion.Enums;
+using OnnxStack.StableDiffusion.Helpers;
+using OnnxStack.StableDiffusion.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OnnxStack.StableDiffusion.Pipelines
+{
+    public sealed class StableCascadePipeline : StableDiffusionPipeline
+    {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StableCascadePipeline"/> class.
+        /// </summary>
+        /// <param name="pipelineOptions">The pipeline options</param>
+        /// <param name="tokenizer">The tokenizer.</param>
+        /// <param name="textEncoder">The text encoder.</param>
+        /// <param name="unet">The unet.</param>
+        /// <param name="vaeDecoder">The vae decoder.</param>
+        /// <param name="vaeEncoder">The vae encoder.</param>
+        /// <param name="logger">The logger.</param>
+        public StableCascadePipeline(PipelineOptions pipelineOptions, TokenizerModel tokenizer, TextEncoderModel textEncoder, UNetConditionModel unet, AutoEncoderModel vaeDecoder, AutoEncoderModel vaeEncoder, List<DiffuserType> diffusers, SchedulerOptions defaultSchedulerOptions = default, ILogger logger = default)
+            : base(pipelineOptions, tokenizer, textEncoder, unet, vaeDecoder, vaeEncoder, diffusers, defaultSchedulerOptions, logger)
+        {
+            _supportedDiffusers = diffusers ?? new List<DiffuserType>
+            {
+                DiffuserType.TextToImage
+            };
+            _supportedSchedulers = new List<SchedulerType>
+            {
+                SchedulerType.EulerAncestral
+            };
+            _defaultSchedulerOptions = defaultSchedulerOptions ?? new SchedulerOptions
+            {
+                InferenceSteps = 1,
+                GuidanceScale = 0f,
+                SchedulerType = SchedulerType.EulerAncestral
+            };
+        }
+
+
+        /// <summary>
+        /// Gets the type of the pipeline.
+        /// </summary>
+        public override DiffuserPipelineType PipelineType => DiffuserPipelineType.StableCascade;
+
+
+        /// <summary>
+        /// Creates the diffuser.
+        /// </summary>
+        /// <param name="diffuserType">Type of the diffuser.</param>
+        /// <param name="controlNetModel">The control net model.</param>
+        /// <returns></returns>
+        protected override IDiffuser CreateDiffuser(DiffuserType diffuserType, ControlNetModel controlNetModel)
+        {
+            return diffuserType switch
+            {
+                DiffuserType.TextToImage => new TextDiffuser(_unet, _vaeDecoder, _vaeEncoder, _pipelineOptions.MemoryMode, _logger),
+                DiffuserType.ControlNet => new ControlNetDiffuser(controlNetModel, _unet, _vaeDecoder, _vaeEncoder, _pipelineOptions.MemoryMode, _logger),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+
+        /// <summary>
+        /// Creates the pipeline from a ModelSet configuration.
+        /// </summary>
+        /// <param name="modelSet">The model set.</param>
+        /// <param name="logger">The logger.</param>
+        /// <returns></returns>
+        public static new StableCascadePipeline CreatePipeline(StableDiffusionModelSet modelSet, ILogger logger = default)
+        {
+            var unet = new UNetConditionModel(modelSet.UnetConfig.ApplyDefaults(modelSet));
+            var tokenizer = new TokenizerModel(modelSet.TokenizerConfig.ApplyDefaults(modelSet));
+            var textEncoder = new TextEncoderModel(modelSet.TextEncoderConfig.ApplyDefaults(modelSet));
+            var vaeDecoder = new AutoEncoderModel(modelSet.VaeDecoderConfig.ApplyDefaults(modelSet));
+            var vaeEncoder = new AutoEncoderModel(modelSet.VaeEncoderConfig.ApplyDefaults(modelSet));
+            var pipelineOptions = new PipelineOptions(modelSet.Name, modelSet.MemoryMode);
+            return new StableCascadePipeline(pipelineOptions, tokenizer, textEncoder, unet, vaeDecoder, vaeEncoder, modelSet.Diffusers, modelSet.SchedulerOptions, logger);
+        }
+
+
+        /// <summary>
+        /// Creates the pipeline from a folder structure.
+        /// </summary>
+        /// <param name="modelFolder">The model folder.</param>
+        /// <param name="modelType">Type of the model.</param>
+        /// <param name="deviceId">The device identifier.</param>
+        /// <param name="executionProvider">The execution provider.</param>
+        /// <param name="logger">The logger.</param>
+        /// <returns></returns>
+        public static new StableCascadePipeline CreatePipeline(string modelFolder, ModelType modelType = ModelType.Base, int deviceId = 0, ExecutionProvider executionProvider = ExecutionProvider.DirectML, MemoryModeType memoryMode = MemoryModeType.Maximum, ILogger logger = default)
+        {
+            return CreatePipeline(ModelFactory.CreateModelSet(modelFolder, DiffuserPipelineType.StableCascade, modelType, deviceId, executionProvider, memoryMode), logger);
+        }
+    }
+}
